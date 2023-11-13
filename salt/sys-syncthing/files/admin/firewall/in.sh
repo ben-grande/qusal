963e72c7ed4a115343eb2baf4fce0f31477d4413
Ben Grande <ben.grande.b@gmail.com>
2023-11-13 18:18:06 +0000

    chore: Fix unman copyright contact

--- salt/sys-syncthing/files/admin/firewall/in.sh
+++ salt/sys-syncthing/files/admin/firewall/in.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# SPDX-FileCopyrightText: 2022 unman <unman@thirdeyesecurity.com>
+# SPDX-FileCopyrightText: 2022 unman <unman@thirdeyesecurity.org>
 # SPDX-FileCopyrightText: 2023 Benjamin Grande M. S. <ben.grande.b@gmail.com>
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
