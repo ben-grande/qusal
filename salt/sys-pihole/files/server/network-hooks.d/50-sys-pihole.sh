6efcc1da77ed2d9783f2cbaa0375a3b7706c0665
Ben Grande <ben.grande.b@gmail.com>
2024-01-29 16:49:54 +0100

    chore: copyright update

--- salt/sys-pihole/files/server/network-hooks.d/50-sys-pihole.sh
+++ salt/sys-pihole/files/server/network-hooks.d/50-sys-pihole.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # SPDX-FileCopyrightText: 2022 - 2023 unman <unman@thirdeyesecurity.org>
-# SPDX-FileCopyrightText: 2023 Benjamin Grande M. S. <ben.grande.b@gmail.com>
+# SPDX-FileCopyrightText: 2023 - 2024 Benjamin Grande M. S. <ben.grande.b@gmail.com>
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
