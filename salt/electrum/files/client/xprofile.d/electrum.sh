f1eec65e67788b1aea6e04f3021643b34119be70
Ben Grande <ben.grande.b@gmail.com>
2024-02-18 17:06:18 +0100

    fix: better electrum GUI resolution and tabs

---
+++ salt/electrum/files/client/xprofile.d/electrum.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+## SPDX-FileCopyrightText: 2024 Benjamin Grande M. S. <ben.grande.b@gmail.com>
+##
+## SPDX-License-Identifier: AGPL-3.0-or-later
+
+current_dpi="$(xrdb -get Xft.dpi)"
+wanted_dpi="144"
+
+if test -z "${current_dpi}" || test "${current_dpi}" -lt "${wanted_dpi}"
+then
+  echo "Xft.dpi: ${wanted_dpi}" | xrdb -override -
+fi
